1e0ab51680941e94cc16db67c82e8b3a9419e6a2
Professor <mh571536@gmail.com>
2024-03-06 15:39:01 +0500

    new commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="bg-white z-10 border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
-          href="https://flowbite.com/"
+          href="#"
           className="flex items-center space-x-3 rtl:space-x-reverse"
         >
           <FaQrcode className="w-8 h-8 text-indigo-500" />
